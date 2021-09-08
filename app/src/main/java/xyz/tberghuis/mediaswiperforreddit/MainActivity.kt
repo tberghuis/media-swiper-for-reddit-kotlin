bc06e4caea1798adabab4cc63b3aff71ad2d159a
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-09-08 18:37:44 +1000

    .

--- app/src/main/java/xyz/tberghuis/mediaswiperforreddit/MainActivity.kt
+++ app/src/main/java/xyz/tberghuis/mediaswiperforreddit/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
