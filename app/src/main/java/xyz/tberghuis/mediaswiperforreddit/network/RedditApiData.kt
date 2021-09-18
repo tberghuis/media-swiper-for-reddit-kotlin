60af38b221bcadf8608cece5d3c64127aa337dc6
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-09-18 16:10:43 +1000

    .

--- app/src/main/java/xyz/tberghuis/mediaswiperforreddit/network/RedditApiData.kt
+++ app/src/main/java/xyz/tberghuis/mediaswiperforreddit/network/RedditApiData.kt
@@ -10,4 +10,4 @@ data class RedditApiPostData(val media: RedditApiMedia?, val url: String)
 
 data class RedditApiMedia(val oembed: RedditApiOembed)
 
-data class RedditApiOembed(val thumbnail_url: String)
+data class RedditApiOembed(val thumbnail_url: String?)
